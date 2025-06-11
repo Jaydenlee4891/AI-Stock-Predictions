a14140c1c5adb750334000728b7a633f60215817
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-11 22:27:06 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,3 +38,9 @@ class TradingBotGUI:
     self.drawdown_entry = tk.Entry(self.form_frame)
     self.drawdown_entry_grid(row=1,column=5)
 
+    #table to track the traded equities
+    self.tree = ttk.Treeview(root,columns = ("Symbol", "Position", "Entry Price", "Levels", "Status"),show = 'headings')
+    for col in ["Symbol", "Position", "Entry Price", "Levels", "Status"]
+      self.tree.heading(col, text=col)
+      self.tree.column(col, witdth =120)
+    self.tree.pack(pady=10)
