40354715397f5235d124e2b8c0bac1b35c2896ce
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-11 22:14:16 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,3 +4,12 @@ import json
 import time
 import threading
 import random
+
+DATA_FILE = "equities.json"
+
+def fetch_mock_api(symbol):
+  return{
+    "price" : 100
+  }
+
+def mock_chatgpt_response
