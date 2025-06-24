d42c391bd221c8948ec3fb138ac08483d61129da
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-24 20:02:57 +0900

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -2,4 +2,4 @@ import alpaca_trade_api as tradeapi
 
 key = "PKBOVFA2LQAB1QPTY5HZ"
 secret_key="U9FDVHrHWtKmPOuAunhXpFqb0mCfLjhzTtE7O7k0"
-BASE_URL="https://paper-api.alpaca.markets"
+BASE_URL="https://paper-api.alpaca.markets/v2"
