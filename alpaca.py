bfe6c0124e2ed69ab3af05efd245654cbbfb7bb2
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-24 20:04:47 +0900

    Update alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -5,3 +5,10 @@ secret_key="U9FDVHrHWtKmPOuAunhXpFqb0mCfLjhzTtE7O7k0"
 BASE_URL="https://paper-api.alpaca.markets/v2"
 
 api = tradeapi.REST(key, secret_key, BASE_URL, api_version = "v2")
+
+def get_data(symbol):
+  try:
+    barset = api.get_latest_trade(symbol)
+    return {"price":braset.price}
+  except Exception as e:
+    return{"price":-1}
