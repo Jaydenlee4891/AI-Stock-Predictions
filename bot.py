fa0c0e5f9249ab35fb257d954c6d171cb4441d84
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-06-30 21:53:03 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,8 @@ import alpaca_trade_api as tradeapi
 
 DATA_FILE = "equities.json"
 
-key = "PKBOVFA2LQAB1QPTY5HZ"
-secret_key="U9FDVHrHWtKmPOuAunhXpFqb0mCfLjhzTtE7O7k0"
+key = ""
+secret_key=""
 BASE_URL="https://paper-api.alpaca.markets/"
 
 api = tradeapi.REST(key, secret_key, BASE_URL, api_version = "v2")
